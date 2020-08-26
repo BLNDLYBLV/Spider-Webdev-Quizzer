104d5bda0faf0f7fd289ed4a39557e1a789b415c
Sriram <sriram01201@gmail.com>
2020-08-27 02:55:44 +0530

    Built start page and started question page

--- work.js
+++ work.js
@@ -1,1 +1,23 @@
-// console.log('hello');
+var startbtn=document.getElementById('startbtn');
+var nameinput=document.getElementById('nameinput');
+var starterror=document.getElementById('starterror');
+var start=document.getElementById('start');
+var quiz=document.getElementById('quiz');
+
+nameinput.value='';
+
+var currentUser;
+
+function startclick(){
+    if(nameinput.value!=''){
+        console.log(nameinput.value);
+        start.style.display='none';
+        currentUser=nameinput.value;
+        quiz.style.display='block';
+    }
+    else{
+        starterror.style.display='block';
+        starterror.innerHTML='*Please fill your name in the form';
+    }
+}
+
